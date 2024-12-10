c25efb4e0279c2371ef1e51fcdae86a9d22cb4e8
castrilloredondovictor <castrilloredondovictor@gmail.com>
2024-12-10 12:49:32 +0100

    Add project files.

---
+++ FundamentosNetCore/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace FundamentosNetCore
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnSubmit = new Button();
+            txtNombre = new TextBox();
+            lblNombre = new Label();
+            lblTexto = new Label();
+            SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            btnSubmit.Location = new Point(56, 122);
+            btnSubmit.Name = "btnSubmit";
+            btnSubmit.Size = new Size(75, 23);
+            btnSubmit.TabIndex = 0;
+            btnSubmit.Text = "Sumbit";
+            btnSubmit.UseVisualStyleBackColor = true;
+            btnSubmit.Click += btnSubmit_Click;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(47, 93);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(100, 23);
+            txtNombre.TabIndex = 1;
+            // 
+            // lblNombre
+            // 
+            lblNombre.AutoSize = true;
+            lblNombre.Location = new Point(56, 75);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(54, 15);
+            lblNombre.TabIndex = 2;
+            lblNombre.Text = "Nombre:";
+            // 
+            // lblTexto
+            // 
+            lblTexto.AutoSize = true;
+            lblTexto.Location = new Point(131, 163);
+            lblTexto.Name = "lblTexto";
+            lblTexto.Size = new Size(0, 15);
+            lblTexto.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblTexto);
+            Controls.Add(lblNombre);
+            Controls.Add(txtNombre);
+            Controls.Add(btnSubmit);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnSubmit;
+        private TextBox txtNombre;
+        private Label lblNombre;
+        private Label lblTexto;
+    }
+}
---
+++ FundamentosNetCore/Form1.cs
@@ -0,0 +1,21 @@
+namespace FundamentosNetCore
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Algo aqui...";
+            this.SuspendLayout();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string nombre = this.txtNombre.Text;
+            this.lblTexto.Text = "Su nombre es: " + nombre;
+            this.lblTexto.Location = new Point(20, 10);
+            this.lblTexto.Width = 130;
+            this.lblTexto.TextAlign = HorizontalAlignment.Right;
+        }
+    }
+}
